6b52ffef6c70bd376dee524f9f9d5e12362fe759
ali emad <aliemadaboshosha13@gmail.com>
2023-03-07 19:59:07 +0200

    uplode the original copy of this animation project from my own PC to My Githup account

---
+++ task1 move with button/animation function.js
@@ -0,0 +1,12 @@
+let section1=document.getElementsByTagName("section")[0];
+function start(){
+    
+    section1.classList.add('section')
+
+}
+
+function stop(){
+    section1.classList.remove('section');
+    
+
+}
